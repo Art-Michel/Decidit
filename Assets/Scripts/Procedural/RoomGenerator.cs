0f4764e2a3dcceb44076fc3528404ca3cb649094
Liropp <69756753+Liropp@users.noreply.github.com>
2022-10-03 11:29:31 +0200

    procedural rooms v2.5

--- Assets/Scripts/Procedural/RoomGenerator.cs
+++ Assets/Scripts/Procedural/RoomGenerator.cs
@@ -32,34 +32,24 @@ public class RoomGenerator : MonoBehaviour
                 switch (randAxe)
                 {
                     case 1:
-                        Debug.Log("x");
+                        //Debug.Log("x");
                         Vector3 SpawnPos1 = new Vector3(previousRoom.x + roomsDist, previousRoom.y, previousRoom.z);
+                        Vector3 NextSpawn1 = new Vector3(previousRoom.x + roomsDist*2, previousRoom.y, previousRoom.z);
 
-                        if (CanSpawn(SpawnPos1))
+                        if (CanSpawn(SpawnPos1) && CanSpawn(NextSpawn1))
                         {
-                            GameObject corridorInstance1 = Instantiate(rooms[6], SpawnPos1, Quaternion.identity);
-                            roomsPlaced.Add(corridorInstance1.transform.position);
-                            previousRoom = corridorInstance1.transform.position;
+                            if(roomsCount < roomsCountMax)
+                            {
+                                GameObject corridorInstance1 = Instantiate(rooms[1], SpawnPos1, Quaternion.identity);
+                                roomsPlaced.Add(corridorInstance1.transform.position);
+                                previousRoom = corridorInstance1.transform.position;
+                            }
 
                             Vector3 SpawnPos1b = new Vector3(previousRoom.x + roomsDist, previousRoom.y, previousRoom.z);
-                            GameObject roomInstance1 = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos1b)], SpawnPos1b, Quaternion.identity);
+                            GameObject roomInstance1 = Instantiate(rooms[0], SpawnPos1b, Quaternion.identity);
                             roomsPlaced.Add(roomInstance1.transform.position);
                             previousRoom = roomInstance1.transform.position;
                             roomsCount--;
-
-                            if (roomsCount > 0)
-                            {
-                                Vector3 SpawnPos1d = new Vector3(previousRoom.x + roomsDist, previousRoom.y, previousRoom.z);
-                                GameObject corridorInstance2 = Instantiate(rooms[6], SpawnPos1d, Quaternion.identity);
-                                roomsPlaced.Add(corridorInstance2.transform.position);
-                                previousRoom = corridorInstance2.transform.position;
-
-                                Vector3 SpawnPos1c = new Vector3(previousRoom.x + roomsDist, previousRoom.y, previousRoom.z);
-                                GameObject nextRoomInstance = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos1c)], SpawnPos1c, Quaternion.identity);
-                                roomsPlaced.Add(nextRoomInstance.transform.position);
-                                previousRoom = nextRoomInstance.transform.position;
-                                roomsCount--;
-                            }
                         }
                         else
                         {
@@ -67,12 +57,12 @@ public class RoomGenerator : MonoBehaviour
                         }
                         break;
                     case 2:
-                        Debug.Log("y");
+                        //Debug.Log("y");
                         Vector3 SpawnPos2 = new Vector3(previousRoom.x, previousRoom.y + roomsDist / 3f, previousRoom.z);
 
                         if (CanSpawn(SpawnPos2))
                         {
-                            GameObject roomInstance2 = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos2)], SpawnPos2, Quaternion.identity);
+                            GameObject roomInstance2 = Instantiate(rooms[0], SpawnPos2, Quaternion.identity);
                             roomsPlaced.Add(roomInstance2.transform.position);
                             previousRoom = roomInstance2.transform.position;
                             roomsCount--;
@@ -83,34 +73,24 @@ public class RoomGenerator : MonoBehaviour
                         }
                         break;
                     case 3:
-                        Debug.Log("z");
+                        //Debug.Log("z");
                         Vector3 SpawnPos3 = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z + roomsDist);
+                        Vector3 NextSpawn3 = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z + roomsDist*2);
 
-                        if (CanSpawn(SpawnPos3))
+                        if (CanSpawn(SpawnPos3) && CanSpawn(NextSpawn3))
                         {
-                            GameObject corridorInstance2 = Instantiate(rooms[7], SpawnPos3, Quaternion.identity);
-                            roomsPlaced.Add(corridorInstance2.transform.position);
-                            previousRoom = corridorInstance2.transform.position;
+                            if (roomsCount < roomsCountMax)
+                            {
+                                GameObject corridorInstance2 = Instantiate(rooms[2], SpawnPos3, Quaternion.identity);
+                                roomsPlaced.Add(corridorInstance2.transform.position);
+                                previousRoom = corridorInstance2.transform.position;
+                            }
 
                             Vector3 SpawnPos3b = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z + roomsDist);
-                            GameObject roomInstance3 = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos3b)], SpawnPos3b, Quaternion.identity);
+                            GameObject roomInstance3 = Instantiate(rooms[0], SpawnPos3b, Quaternion.identity);
                             roomsPlaced.Add(roomInstance3.transform.position);
                             previousRoom = roomInstance3.transform.position;
                             roomsCount--;
-
-                            if (roomsCount > 0)
-                            {
-                                Vector3 SpawnPos3d = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z + roomsDist);
-                                GameObject corridorInstance3 = Instantiate(rooms[7], SpawnPos3d, Quaternion.identity);
-                                roomsPlaced.Add(corridorInstance3.transform.position);
-                                previousRoom = corridorInstance3.transform.position;
-
-                                Vector3 SpawnPos3c = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z + roomsDist);
-                                GameObject nextRoomInstance = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos3c)], SpawnPos3c, Quaternion.identity);
-                                roomsPlaced.Add(nextRoomInstance.transform.position);
-                                previousRoom = nextRoomInstance.transform.position;
-                                roomsCount--;
-                            }
                         }
                         else
                         {
@@ -118,34 +98,24 @@ public class RoomGenerator : MonoBehaviour
                         }
                         break;
                     case 4:
-                        Debug.Log("-z");
+                        //Debug.Log("-z");
                         Vector3 SpawnPos4 = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z - roomsDist);
+                        Vector3 NextSpawn4 = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z - roomsDist*2);
 
-                        if (CanSpawn(SpawnPos4))
+                        if (CanSpawn(SpawnPos4) && CanSpawn(NextSpawn4))
                         {
-                            GameObject corridorInstance2 = Instantiate(rooms[7], SpawnPos4, Quaternion.identity);
-                            roomsPlaced.Add(corridorInstance2.transform.position);
-                            previousRoom = corridorInstance2.transform.position;
+                            if (roomsCount < roomsCountMax)
+                            {
+                                GameObject corridorInstance2 = Instantiate(rooms[2], SpawnPos4, Quaternion.identity);
+                                roomsPlaced.Add(corridorInstance2.transform.position);
+                                previousRoom = corridorInstance2.transform.position;
+                            }
 
                             Vector3 SpawnPos4b = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z - roomsDist);
-                            GameObject roomInstance4 = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos4b)], SpawnPos4b, Quaternion.identity);
+                            GameObject roomInstance4 = Instantiate(rooms[0], SpawnPos4b, Quaternion.identity);
                             roomsPlaced.Add(roomInstance4.transform.position);
                             previousRoom = roomInstance4.transform.position;
                             roomsCount--;
-
-                            if (roomsCount > 0)
-                            {
-                                Vector3 SpawnPos4d = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z - roomsDist);
-                                GameObject corridorInstance3 = Instantiate(rooms[7], SpawnPos4d, Quaternion.identity);
-                                roomsPlaced.Add(corridorInstance3.transform.position);
-                                previousRoom = corridorInstance3.transform.position;
-
-                                Vector3 SpawnPos4c = new Vector3(previousRoom.x, previousRoom.y, previousRoom.z - roomsDist);
-                                GameObject nextRoomInstance = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos4c)], SpawnPos4c, Quaternion.identity);
-                                roomsPlaced.Add(nextRoomInstance.transform.position);
-                                previousRoom = nextRoomInstance.transform.position;
-                                roomsCount--;
-                            }
                         }
                         else
                         {
@@ -153,34 +123,24 @@ public class RoomGenerator : MonoBehaviour
                         }
                         break;
                     case 5:
-                        Debug.Log("-x");
+                        //Debug.Log("-x");
                         Vector3 SpawnPos5 = new Vector3(previousRoom.x - roomsDist, previousRoom.y, previousRoom.z);
+                        Vector3 NextSpawn5 = new Vector3(previousRoom.x - roomsDist*2, previousRoom.y, previousRoom.z);
 
-                        if (CanSpawn(SpawnPos5))
+                        if (CanSpawn(SpawnPos5) && CanSpawn(NextSpawn5))
                         {
-                            GameObject corridorInstance1 = Instantiate(rooms[6], SpawnPos5, Quaternion.identity);
-                            roomsPlaced.Add(corridorInstance1.transform.position);
-                            previousRoom = corridorInstance1.transform.position;
+                            if (roomsCount < roomsCountMax)
+                            {
+                                GameObject corridorInstance1 = Instantiate(rooms[1], SpawnPos5, Quaternion.identity);
+                                roomsPlaced.Add(corridorInstance1.transform.position);
+                                previousRoom = corridorInstance1.transform.position;
+                            }
 
                             Vector3 SpawnPos5b = new Vector3(previousRoom.x - roomsDist, previousRoom.y, previousRoom.z);
-                            GameObject roomInstance5 = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos5b)], SpawnPos5b, Quaternion.identity);
+                            GameObject roomInstance5 = Instantiate(rooms[0], SpawnPos5b, Quaternion.identity);
                             roomsPlaced.Add(roomInstance5.transform.position);
                             previousRoom = roomInstance5.transform.position;
                             roomsCount--;
-
-                            if (roomsCount > 0)
-                            {
-                                Vector3 SpawnPos5d = new Vector3(previousRoom.x - roomsDist, previousRoom.y, previousRoom.z);
-                                GameObject corridorInstance2 = Instantiate(rooms[6], SpawnPos5d, Quaternion.identity);
-                                roomsPlaced.Add(corridorInstance2.transform.position);
-                                previousRoom = corridorInstance2.transform.position;
-
-                                Vector3 SpawnPos5c = new Vector3(previousRoom.x - roomsDist, previousRoom.y, previousRoom.z);
-                                GameObject nextRoomInstance = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos5c)], SpawnPos5c, Quaternion.identity);
-                                roomsPlaced.Add(nextRoomInstance.transform.position);
-                                previousRoom = nextRoomInstance.transform.position;
-                                roomsCount--;
-                            }
                         }
                         else
                         {
@@ -188,12 +148,12 @@ public class RoomGenerator : MonoBehaviour
                         }
                         break;
                     case 6:
-                        Debug.Log("-y");
+                        //Debug.Log("-y");
                         Vector3 SpawnPos6 = new Vector3(previousRoom.x, previousRoom.y - roomsDist / 3f, previousRoom.z);
 
                         if (CanSpawn(SpawnPos6))
                         {
-                            GameObject roomInstance6 = Instantiate(rooms[ChooseRoomDir(previousRoom, SpawnPos6)], SpawnPos6, Quaternion.identity);
+                            GameObject roomInstance6 = Instantiate(rooms[0], SpawnPos6, Quaternion.identity);
                             roomsPlaced.Add(roomInstance6.transform.position);
                             previousRoom = roomInstance6.transform.position;
                             roomsCount--;
---
+++ Assets/Scripts/Procedural/WallStates.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallStates : MonoBehaviour
+{
+    bool canChangeWallState = true;
+
+    private void Update()
+    {
+        if (canChangeWallState)
+        {
+            Collider[] hit = Physics.OverlapSphere(transform.position, 1f);
+            if (hit.Length > 0)
+            {
+                for (int i = 0; i < hit.Length; i++)
+                {
+                    hit[i].transform.gameObject.SetActive(false);
+                    canChangeWallState = false;
+                }
+            }
+        }
+    }
+}
