dda46f64318422221240523c3dc531e3a9bce917
Art_Desktop <asheanx@gmail.com>
2022-12-06 19:34:53 +0100

    réparé tout

--- Assets/Scripts/Hitboxes/Hitbox.cs
+++ Assets/Scripts/Hitboxes/Hitbox.cs
@@ -64,7 +64,6 @@ public class Hitbox : MonoBehaviour
             targetCollider.parent.GetComponent<Health>().TakeCriticalDamage(_damage);
         else
         {
-            Debug.Log(targetCollider.name);
             targetCollider.parent.GetComponent<Health>().TakeDamage(_damage);
         }
         _blacklist.Add(targetCollider.parent, _delayBetweenHits);
