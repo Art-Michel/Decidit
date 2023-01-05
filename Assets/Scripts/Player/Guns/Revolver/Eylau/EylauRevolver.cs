a58da7d28d8750d43950010c74ba3305058e0146
Art_Desktop <asheanx@gmail.com>
2023-01-05 22:49:37 +0100

    eylau charge 2

--- Assets/Scripts/Player/Guns/Revolver/Eylau/EylauRevolver.cs
+++ Assets/Scripts/Player/Guns/Revolver/Eylau/EylauRevolver.cs
@@ -10,22 +10,70 @@ using NaughtyAttributes;
 public class EylauRevolver : Revolver
 {
     [Foldout("References")]
+    [SerializeField] private Pooler _projectile0Pooler;
+    [Foldout("References")]
     [SerializeField] private Pooler _projectile1Pooler;
+    [Foldout("References")]
+    [SerializeField] private Pooler _projectile2Pooler;
+    [Foldout("References")]
+    [SerializeField] private Pooler _projectile3Pooler;
+    [Foldout("References")]
+    [SerializeField] private Pooler _projectile4Pooler;
+    [Foldout("References")]
+    [SerializeField] private Pooler _projectile5Pooler;
     [Foldout("References")]
     [SerializeField] private Image _chargeUi;
 
-    // [Foldout("Recoils")]
-    // [SerializeField] private float _recoilCharge0 = .9f;
-    // [Foldout("Recoils")]
-    // [SerializeField] private float _recoilCharge1 = 1f;
-    // [Foldout("Recoils")]
-    // [SerializeField] private float _recoilCharge2 = 1.1f;
-    // [Foldout("Recoils")]
-    // [SerializeField] private float _recoilCharge3 = 1.2f;
-    // [Foldout("Recoils")]
-    // [SerializeField] private float _recoilCharge4 = 1.3f;
-    // [Foldout("Recoils")]
-    // [SerializeField] private float _recoilCharge5 = 1.6f;
+    [Foldout("Recoils values")]
+    [SerializeField] private float _additionalRecoilCharge0 = .1f;
+    [Foldout("Recoils values")]
+    [SerializeField] private float _additionalRecoilCharge1 = .2f;
+    [Foldout("Recoils values")]
+    [SerializeField] private float _additionalRecoilCharge2 = .3f;
+    [Foldout("Recoils values")]
+    [SerializeField] private float _additionalRecoilCharge3 = .4f;
+    [Foldout("Recoils values")]
+    [SerializeField] private float _additionalRecoilCharge4 = .5f;
+    [Foldout("Recoils values")]
+    [SerializeField] private float _additionalRecoilCharge5 = .6f;
+
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeIntensity0 = .9f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeIntensity1 = 1f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeIntensity2 = 1.1f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeIntensity3 = 1.2f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeIntensity4 = 1.3f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeIntensity5 = 1.6f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeDuration0 = .9f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeDuration1 = 1f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeDuration2 = 1.1f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeDuration3 = 1.2f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeDuration4 = 1.3f;
+    [Foldout("Screenshake values")]
+    [SerializeField] private float _shakeDuration5 = 1.6f;
+
+    [Foldout("Projectiles")]
+    [SerializeField] private GameObject _bulletPrefab0;
+    [Foldout("Projectiles")]
+    [SerializeField] private GameObject _bulletPrefab1;
+    [Foldout("Projectiles")]
+    [SerializeField] private GameObject _bulletPrefab2;
+    [Foldout("Projectiles")]
+    [SerializeField] private GameObject _bulletPrefab3;
+    [Foldout("Projectiles")]
+    [SerializeField] private GameObject _bulletPrefab4;
+    [Foldout("Projectiles")]
+    [SerializeField] private GameObject _bulletPrefab5;
 
     [Foldout("Stats")]
     [SerializeField] private float _chargeSpeed = 2f;
@@ -47,10 +95,38 @@ public class EylauRevolver : Revolver
 
     public override void Shoot()
     {
-        PooledObject shot = _projectile1Pooler.Get();
-        shot.GetComponent<Projectile>().Setup(_canonPosition.position, (_currentlyAimedAt - _canonPosition.position).normalized);
+        PooledObject shot = null;
+        switch (_currentChargeStep)
+        {
+            case 0:
+                shot = _projectile0Pooler.Get();
+                Player.Instance.StartShake(_shakeIntensity0, _shakeDuration0);
+                break;
+            case 1:
+                shot = _projectile1Pooler.Get();
+                Player.Instance.StartShake(_shakeIntensity1, _shakeDuration1);
+                break;
+            case 2:
+                shot = _projectile2Pooler.Get();
+                Player.Instance.StartShake(_shakeIntensity2, _shakeDuration2);
+                break;
+            case 3:
+                shot = _projectile3Pooler.Get();
+                Player.Instance.StartShake(_shakeIntensity3, _shakeDuration3);
+                break;
+            case 4:
+                shot = _projectile4Pooler.Get();
+                Player.Instance.StartShake(_shakeIntensity4, _shakeDuration4);
+                break;
+            case 5:
+                //Laser
+                Player.Instance.StartShake(_shakeIntensity5, _shakeDuration5);
+                break;
+        }
 
-        Player.Instance.StartShake(_shootShakeIntensity, _shootShakeDuration);
+        if (shot != null)
+            shot.GetComponent<Projectile>().Setup(_canonPosition.position, (_currentlyAimedAt - _canonPosition.position).normalized);
+
         PlaceHolderSoundManager.Instance.PlayEylauShot(_currentChargeStep);
         ResetChargeLevel();
         _muzzleFlash.Play();
--- Assets/Scripts/VFX/TwoPosTrail.cs
+++ Assets/Scripts/VFX/TwoPosTrail.cs
@@ -8,13 +8,24 @@ public class TwoPosTrail : PooledObject
     [SerializeField] Transform _startObj;
     [SerializeField] Transform _endObj;
     [SerializeField] VisualEffect _vfx;
+    private float _cd;
+
     public void SetPos(Vector3 startPos, Vector3 endPos)
     {
         transform.position = startPos;
         _endObj.localPosition = endPos;
     }
+
     public void Play()
     {
         _vfx.Play();
+        _cd = .4f;
+    }
+
+    void Update()
+    {
+        _cd -= Time.deltaTime;
+        if (_cd <= 0f)
+            this.Pooler.Return(this);
     }
 }
