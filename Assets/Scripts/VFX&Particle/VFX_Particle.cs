6933cd219434c8c620cef70b86b1c68172e2e0b8
Rafromau <raphael.jorand@e-artsup.net>
2023-01-11 16:11:41 +0100

    muse explo roquette fini, fugue warmup buff

---
+++ Assets/Scripts/VFX&Particle/VFX_Particle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using NaughtyAttributes;
+
+public class VFX_Particle : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect _vfx;
+    [SerializeField]
+    private List<ParticleSystem> _particleSystems;
+
+    [Button]
+    private void PlayAll()
+    {
+        _vfx.Play();
+        foreach (ParticleSystem system in _particleSystems)
+            system.Play();
+    }
+}
