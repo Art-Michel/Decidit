99fde14a60961eded5af73e788741597f0888084
Liropp <69756753+Liropp@users.noreply.github.com>
2023-01-31 12:24:26 +0100

    push pour sauver les ia

--- Assets/Scripts/LD/Test_LD_Room.cs
+++ Assets/Scripts/LD/Test_LD_Room.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Test_LD_Room : MonoBehaviour
 {
     public Transform spawn;
+    [SerializeField] private NavMeshSurface[] navMsS;
+
+    private void Awake()
+    {
+        if(navMsS.Length > 0)
+        {
+            foreach (NavMeshSurface navMS in navMsS)
+            {
+                navMS.BuildNavMesh();
+            }
+        }
+    }
 }
