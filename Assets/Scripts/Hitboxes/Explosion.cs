1108786bd9c3085caaf95739d5fca29703856870
Art_Desktop <asheanx@gmail.com>
2023-02-10 13:55:53 +0100

    corrigé explosions qui durent trop longtemps

--- Assets/Scripts/Hitboxes/Explosion.cs
+++ Assets/Scripts/Hitboxes/Explosion.cs
@@ -1,22 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class Explosion : Hitbox
 {
+    [Foldout("Explosion properties")]
+    [SerializeField] private float _lifeSpan = 1f;
+    [Foldout("Explosion properties")]
+    [SerializeField] private float _hitboxSpan = .2f;
+    [Foldout("Explosion properties")]
+    [SerializeField] private float _shakeIntensity = 1;
+    [Foldout("Explosion properties")]
+    [SerializeField] private float _shakeRange = 50f;
+    [Foldout("Explosion properties")]
+    [SerializeField] private float _shakeDuration = .5f;
+
+    [Foldout("Explosion References")]
+    [SerializeField] private GameObject _windBox;
+    [Foldout("Explosion References")]
+    [SerializeField] private VFX_Particle _explosionVfx;
+    [Foldout("Explosion References")]
+    [SerializeField] private AudioSource _explosionAudioSource;
+    [Foldout("Explosion References")]
+    [SerializeField] private AudioClip _explosionSfx;
+
+
+    private bool _hitboxIsActive;
     private Projectile _parentProjectile;
     private float _lifeT;
+    private float _hitboxT;
 
     private float _initialKnockbackForce;
     private float _initialDamage;
-    [SerializeField] private float _lifeSpan = 1f;
-    [SerializeField] private VFX_Particle _explosionVfx;
-    [SerializeField] private AudioSource _explosionAudioSource;
-    [SerializeField] private AudioClip _explosionSfx;
-    [SerializeField] private float _shakeIntensity;
-    [SerializeField] private float _shakeRange;
-    [SerializeField] private float _shakeDuration;
 
     protected override void Awake()
     {
@@ -38,7 +56,12 @@ public class Explosion : Hitbox
 
     private void Reset()
     {
+        if (_windBox)
+            _windBox.SetActive(true);
+
+        _hitboxIsActive = true;
         _lifeT = _lifeSpan;
+        _hitboxT = _hitboxSpan;
         _knockbackForce = _initialKnockbackForce;
         _damage = (int)_initialDamage;
         PlayMuseExplosion();
@@ -62,20 +85,35 @@ public class Explosion : Hitbox
 
     protected override void Update()
     {
-        base.Update();
+
+        //Object life span (equal to duration of the VFX)
         _lifeT -= Time.deltaTime;
         if (_lifeT <= 0)
         {
             gameObject.SetActive(false);
-            // if (_explosionVfx)
-            // {
-            //     _explosionVfx.PlayAll();
-            // }
             _parentProjectile.Disappear();
         }
 
+        //Explosion life span
+        _hitboxT -= Time.deltaTime;
+        if (_hitboxT >= 0 && _hitboxIsActive)
+        {
+            DisableHitboxes();
+        }
+
         //Explosion gets weaker over time
-        _knockbackForce = _initialKnockbackForce * Mathf.InverseLerp(_lifeSpan, 1f, _lifeT);
-        _damage = Mathf.RoundToInt(_initialDamage * Mathf.InverseLerp(_lifeSpan, 1f, _lifeT));
+        _knockbackForce = _initialKnockbackForce * Mathf.InverseLerp(0f, _hitboxSpan, _hitboxT);
+        _damage = Mathf.RoundToInt(_initialDamage * Mathf.InverseLerp(0f, _hitboxSpan, _hitboxT));
+
+        //Check for collisions with hitbox
+        if (_hitboxIsActive)
+            base.Update();
+    }
+
+    private void DisableHitboxes()
+    {
+        _hitboxIsActive = false;
+        if (_windBox)
+            _windBox.SetActive(false);
     }
 }
