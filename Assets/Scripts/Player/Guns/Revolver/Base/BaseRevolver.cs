e19c34b76f7ed8819610209c7928b5190ac75680
Art_Laptop <asheanx@gmail.com>
2023-02-14 15:34:39 +0100

    Bullet impacts

--- Assets/Scripts/Player/Guns/Revolver/Base/BaseRevolver.cs
+++ Assets/Scripts/Player/Guns/Revolver/Base/BaseRevolver.cs
@@ -8,7 +8,11 @@ using UnityEngine.VFX;
 public class BaseRevolver : Revolver
 {
     [Foldout("References")]
-    [SerializeField] Pooler _vfxPooler;
+    [SerializeField] Pooler _trailVfxPooler;
+    [Foldout("References")]
+    [SerializeField] Pooler _impactVfxPooler;
+    [Foldout("References")]
+    [SerializeField] Pooler _flashSplashVfxPooler;
 
     [Foldout("Stats")]
     [SerializeField] int _hitscanDamage = 25;
@@ -17,15 +21,37 @@ public class BaseRevolver : Revolver
 
     public override void Shoot()
     {
-        var vfx = _vfxPooler.Get().GetComponent<TwoPosTrail>();
-        if (Physics.Raycast(_camera.position, _camera.forward, out RaycastHit hit, _hitscanMaxRange, _mask) && hit.transform.parent.TryGetComponent<Health>(out Health health))
+        var vfx = _trailVfxPooler.Get().GetComponent<TwoPosTrail>();
+        if (Physics.Raycast(_camera.position, _camera.forward, out RaycastHit hit, _hitscanMaxRange, _mask))
         {
             vfx.SetPos(_canonPosition.position, hit.point);
-            if (hit.transform.CompareTag("WeakHurtbox"))
-                (health as EnemyHealth).TakeCriticalDamage(_hitscanDamage, hit.point, hit.normal);
-            else
-                (health as EnemyHealth).TakeDamage(_hitscanDamage, hit.point, hit.normal);
+
+            //wall or ground
+            if (hit.transform.gameObject.layer == 9)
+            {
+                GameObject impactVfx = _impactVfxPooler.Get().gameObject;
+                impactVfx.transform.position = hit.point + hit.normal * 0.05f;
+                impactVfx.transform.forward = -hit.normal;
+            }
+
+            //flesh
+            if (hit.transform.gameObject.layer == 18)
+            {
+                GameObject splashVfx = _flashSplashVfxPooler.Get().gameObject;
+                splashVfx.transform.position = hit.point + hit.normal * 0.05f;
+                splashVfx.transform.forward = hit.normal;
+            }
+
+            // = enemy hurtbox
+            else if (hit.transform.gameObject.layer == 15 && hit.transform.parent.TryGetComponent<Health>(out Health health))
+            {
+                if (hit.transform.CompareTag("WeakHurtbox"))
+                    (health as EnemyHealth).TakeCriticalDamage(_hitscanDamage, hit.point, hit.normal);
+                else
+                    (health as EnemyHealth).TakeDamage(_hitscanDamage, hit.point, hit.normal);
+            }
         }
+
         else
             vfx.SetPos(_canonPosition.position, _canonPosition.position + _camera.forward * _hitscanMaxRange);
 
---
+++ Assets/Scripts/Pooling/PooledVfx.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooledVfx : PooledObject
+{
+    [SerializeField] float _lifeSpan;
+    [SerializeField] float _lifeT;
+
+    void OnEnable()
+    {
+        _lifeT = _lifeSpan;
+    }
+
+    void Update()
+    {
+        _lifeT -= Time.deltaTime;
+        if (_lifeT <= 0)
+            this.Pooler.Return(this);
+    }
+
+}
