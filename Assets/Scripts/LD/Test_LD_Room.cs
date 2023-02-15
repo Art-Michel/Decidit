f7ba5a9ee82d20ca9b14c642c800466b22a6a42a
Liropp <69756753+Liropp@users.noreply.github.com>
2023-02-15 14:29:07 +0100

    modif sur mini ld

--- Assets/Scripts/LD/Test_LD_Room.cs
+++ Assets/Scripts/LD/Test_LD_Room.cs
@@ -4,16 +4,4 @@ using UnityEngine.AI;
 public class Test_LD_Room : MonoBehaviour
 {
     public Transform spawn;
-    [SerializeField] private NavMeshSurface[] navMsS;
-
-    private void OnEnable()
-    {
-        if (navMsS.Length > 0)
-        {
-            foreach (NavMeshSurface navMS in navMsS)
-            {
-                navMS.BuildNavMesh();
-            }
-        }
-    }
 }
