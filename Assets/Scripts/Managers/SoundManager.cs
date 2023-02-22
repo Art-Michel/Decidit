d27d495680722d25912423bf71bcaea4f615d40d
Art_Laptop <asheanx@gmail.com>
2023-02-22 13:20:48 +0100

    audio

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -7,14 +7,14 @@ public class SoundManager : LocalManager<SoundManager>
 {
     [SerializeField] StudioListener AudioListener;
 
-    protected void Awake()
+    protected override void Awake()
     {
         base.Awake();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
