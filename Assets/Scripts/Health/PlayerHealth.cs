32a3312d13d6f42eceb21b8cadb5bc861fe96740
Art_Laptop <asheanx@gmail.com>
2023-03-02 18:49:53 +0100

    Vignette low hp finie

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -12,6 +12,12 @@ public class PlayerHealth : Health
     [SerializeField] RectTransform _hpBarStart;
     [Foldout("References")]
     [SerializeField] RectTransform _hpBarEnd;
+    [Foldout("References")]
+    [SerializeField] Image _lowHpVignette;
+    [Foldout("References")]
+    [SerializeField] Image _probVignette;
+    [Foldout("References")]
+    [SerializeField] Image _healVignette;
 
     [Foldout("Stats")]
     [SerializeField]
@@ -53,9 +59,17 @@ public class PlayerHealth : Health
         ////PlaceHolderSoundManager.Instance.PlayHurt();
         ////SoundManager.Instance.PlaySound("event:/SFX_Controller/Shoots/HitMarker", 1f);
         //cool magic numbers
-        float shakeIntensity = _playerHurtShakeMaxStrength * Mathf.InverseLerp(0, 40, amount + 10);
+        float shakeIntensity = _playerHurtShakeMaxStrength * Mathf.InverseLerp(0.0f, 40.0f, amount + 10.0f);
         Player.Instance.StartShake(shakeIntensity, _playerHurtShakeDuration);
 
+        HandleLowHpVignette();
+    }
+
+    private void HandleLowHpVignette()
+    {
+        //* vignette starts being visible at [25%]HP and is at full opacity at [10%]hp
+        float value = Mathf.Lerp(1.0f, 0.0f, Mathf.InverseLerp(_maxHp * 0.1f, _maxHp * 0.25f, _hp));
+        _lowHpVignette.color = new Color(1.0f, 1.0f, 1.0f, value);
     }
 
     public override void Knockback(Vector3 direction)
