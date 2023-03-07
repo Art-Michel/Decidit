ccfd4587b075069eadae7d87636d27f174360a43
Art_Laptop <asheanx@gmail.com>
2023-03-07 15:01:48 +0100

    ajusté guns

--- Assets/Scripts/Player/Guns/Revolver/Aragon/AragonRevolver.cs
+++ Assets/Scripts/Player/Guns/Revolver/Aragon/AragonRevolver.cs
@@ -9,27 +9,29 @@ public class AragonRevolver : Revolver
     [Foldout("References")]
     [SerializeField] Pooler _pooler;
 
-    [Foldout("Directions")]
+    [Foldout("BulletsDirection")]
     [SerializeField] bool _shotsAreCentered;
-    [Foldout("Directions")]
+    [Foldout("BulletsDirection")]
     [SerializeField] Vector3[] _shotsDirections;
+    [Foldout("BulletsDirection")]
+    [SerializeField] float[] _offsets;
 
     public override void Shoot()
     {
-        foreach (Vector3 direction in _shotsDirections)
-            SetShot(direction);
+        for (int i = 0; i < _shotsDirections.Length; i++)
+            SetShot(_shotsDirections[i], _offsets[i]);
 
         Player.Instance.StartShake(_shootShakeIntensity, _shootShakeDuration);
         SoundManager.Instance.PlaySound("event:/SFX_Controller/Shoots/FugueAragon/BaseShoot", 1f, gameObject);
         _muzzleFlash.PlayAll();
     }
 
-    private void SetShot(Vector3 direction)
+    private void SetShot(Vector3 direction, float offsetLength)
     {
         direction = direction.normalized;
 
         PooledObject shot = _pooler.Get();
         shot.GetComponent<Projectile>().Setup(_canonPosition.position, (_currentlyAimedAt - _canonPosition.position).normalized, _camera.forward);
-        shot.GetComponent<ProjectileOscillator>().Setup(direction, _shotsAreCentered);
+        shot.GetComponent<ProjectileOscillator>().Setup(direction, _shotsAreCentered, offsetLength);
     }
 }
