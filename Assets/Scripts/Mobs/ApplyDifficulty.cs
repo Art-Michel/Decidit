df9f1e8c1099701a4e27937d36ad9dfb2c95ed48
jeantony <72211385+jeantony@users.noreply.github.com>
2023-03-13 11:47:24 +0100

    slider Diffiulty

--- Assets/Scripts/Mobs/ApplyDifficulty.cs
+++ Assets/Scripts/Mobs/ApplyDifficulty.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ApplyDifficulty : MonoBehaviour
 {
     static public ApplyDifficulty instance;
+    [Range((int)0, (int)2)]
     public int indexDifficulty; // 0 ez / 1 Med / 2 Hard
 
     private void Awake()
