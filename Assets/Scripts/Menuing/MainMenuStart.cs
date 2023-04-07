8c83cba7d1de1c308b4e478d6127f101014c9c45
Art_Laptop <asheanx@gmail.com>
2023-04-07 13:32:49 +0200

    correction timescale

--- Assets/Scripts/Menuing/MainMenuStart.cs
+++ Assets/Scripts/Menuing/MainMenuStart.cs
@@ -7,6 +7,7 @@ public class MainMenuStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1.0f;
         MenuManager.Instance.StartMenuing();
     }
 }
