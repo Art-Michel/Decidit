d9b133c0222fa649482ae73c2b757d9aa1ca393f
GitMaaaan <chaperonlucas@hotmail.fr>
2023-04-18 17:04:04 +0200

    Tout est fait pour les options sauf l'aim assist

--- Assets/Scripts/Menuing/PlayerSettings.cs
+++ Assets/Scripts/Menuing/PlayerSettings.cs
@@ -9,14 +9,24 @@ using NaughtyAttributes;
 public class PlayerSettings : MonoBehaviour
 {
     #region VolumeSettingsVariablesFmod
-    FMOD.Studio.Bus Master;
-    [SerializeField] float MasterVolumeNumber = 1f;
+    FMOD.Studio.Bus master;
+    [SerializeField] float masterVolumeNumber = 1f;
     FMOD.Studio.Bus SFX;
     [SerializeField] float SFXVolumeNumber = 1f;
-    FMOD.Studio.Bus Music;
-    [SerializeField] float MusicVolumeNumber = 1f;
+    FMOD.Studio.Bus music;
+    [SerializeField] float musicVolumeNumber = 1f;
     #endregion
 
+    #region ResolutionsVariables
+    Resolution[] DisponibleResolutions;
+    [SerializeField] Dropdown resolutionDropdown;
+    #endregion
+
+    #region SensivityVariables
+    float xSensivity = Mathf.Clamp(25f,0f,100f);
+    float ySensivity = Mathf.Clamp(25f,0f,100f);
+
+    #endregion
 
     private void Awake()
     {
@@ -24,12 +34,19 @@ public class PlayerSettings : MonoBehaviour
         LoadBus();
 
         //Debug qui affiche la current value du son (Tu peux l'enlever quand tu aura fini)
-        Master.getVolume(out MasterVolumeNumber);
+        master.getVolume(out masterVolumeNumber);
         SFX.getVolume(out SFXVolumeNumber);
-        Music.getVolume(out MusicVolumeNumber);
+        music.getVolume(out musicVolumeNumber);
 
         //chargement des settings
         LoadPrefs();
+
+        //Ca fait en sorte que le résolution soit automatiquement celle de l'écran
+        DisponibleResolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        //Set up des résolutions dispo
+        ResolutionsSetUp();
     }
     // Start is called before the first frame update
     void Start()
@@ -45,8 +62,8 @@ public class PlayerSettings : MonoBehaviour
     #region VolumeFonctions
     public void MasterVolumeUpdate()//ToDoArt un slider qui appele la fonction connecté a la valeur MasterVolumeNumber
     {
-        Master.setVolume(MasterVolumeNumber);//1 = 0Db donc la valeur normale donc 0 = plus de son
-        Master.getVolume(out MasterVolumeNumber);//Debug qui affiche la current value du son
+        master.setVolume(masterVolumeNumber);//1 = 0Db donc la valeur normale donc 0 = plus de son
+        master.getVolume(out masterVolumeNumber);//Debug qui affiche la current value du son
         SavePrefs();
 
     }
@@ -57,23 +74,25 @@ public class PlayerSettings : MonoBehaviour
     }
     public void MusicVolumeUpdate()//ToDoArt un slider qui appele la fonction connecté a la valeur MusicVolumeNumber
     {
-        Music.setVolume(MusicVolumeNumber);//1 = 0Db donc la valeur normale donc 0 = plus de son
-        Music.getVolume(out MusicVolumeNumber);//Debug qui affiche la current value du son
+        music.setVolume(musicVolumeNumber);//1 = 0Db donc la valeur normale donc 0 = plus de son
+        music.getVolume(out musicVolumeNumber);//Debug qui affiche la current value du son
     }
     #endregion
+
+    #region SavingProcess
     public void SavePrefs()
     {
         //TMTC ça save
-        PlayerPrefs.SetFloat("MasterBaseVolume", MasterVolumeNumber);
+        PlayerPrefs.SetFloat("MasterBaseVolume", masterVolumeNumber);
         PlayerPrefs.SetFloat("SFXBaseVolume", SFXVolumeNumber);
-        PlayerPrefs.SetFloat("MusicBaseVolume", MusicVolumeNumber);
+        PlayerPrefs.SetFloat("MusicBaseVolume", musicVolumeNumber);
         PlayerPrefs.Save();
     }
     public void LoadPrefs()
     {
-        MasterVolumeNumber = PlayerPrefs.GetFloat("MasterBaseVolume",MasterVolumeNumber);
+        masterVolumeNumber = PlayerPrefs.GetFloat("MasterBaseVolume", masterVolumeNumber);
         SFXVolumeNumber = PlayerPrefs.GetFloat("SFXBaseVolume", SFXVolumeNumber);
-        MusicVolumeNumber = PlayerPrefs.GetFloat("MusicBaseVolume", MusicVolumeNumber);
+        musicVolumeNumber = PlayerPrefs.GetFloat("MusicBaseVolume", musicVolumeNumber);
     }
 
     [Button]
@@ -81,14 +100,71 @@ public class PlayerSettings : MonoBehaviour
     {
         PlayerPrefs.DeleteAll();
     }
+    #endregion
 
-    public void LoadBus()
+    #region ResolutionFonctions
+    public void ScreenResolution(int ResolutionIndex) //Foction à appeller pour set la resolution
     {
-        Master = FMODUnity.RuntimeManager.GetBus("bus:/");
-        SFX = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
-        Music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        Resolution resolution = DisponibleResolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
+    public void ResolutionsSetUp()
+    {
+        //A renseigner dans l'editor à la main (les differentes résolutions) JE NE PEUX PAS FAIRE PLUS PRORE UNITY DE MERDE
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < DisponibleResolutions.Length; i++)
+        {
+            //Rentre les valleurs dans DisponibleResolutions
+            string option = DisponibleResolutions[i].width + "x" + DisponibleResolutions[i].height;
+            options.Add(option);
+
+            //Sécurité
+            if (DisponibleResolutions[i].width == Screen.currentResolution.width && DisponibleResolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        //Aplique la resolution en temps réel 
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void fullScreenFonction(bool isFullScreen) //fonction pour faire en sorte que le FS soit activable ou non
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+    #endregion
+
+    #region SensivityFonctions
+    public void ChangeMouseSensivity(float x, float y)
+    {
+        xSensivity = x;
+        ySensivity = y;
+        //ta fonction avec xSensivity pour x et ySensivity pour y
+    }
+
+    public void ChangeControllerSensivity(float x, float y)
+    {
+        xSensivity = x;
+        ySensivity = y;
+        //ta fonction avec xSensivity pour x et ySensivity pour y
+    }
+    #endregion
+
+    public void AimAssistChange()
+    {
+        
+    }
+    public void LoadBus()
+    {
+        master = FMODUnity.RuntimeManager.GetBus("bus:/");
+        SFX = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+        music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+    }
     private void OnApplicationQuit()
     {
         SavePrefs();//babysitting anti con du Alt+F4
