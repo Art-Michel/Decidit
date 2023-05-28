f20683fa4d8825cea405b0caf27e2dd2c66f18a2
jeantony <72211385+jeantony@users.noreply.github.com>
2023-05-28 18:19:51 +0200

    timer miliscd ?

--- Assets/Scripts/Timer/DisplayTimer.cs
+++ Assets/Scripts/Timer/DisplayTimer.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using System;
 
 public class DisplayTimer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI txt;
     PlayerInputMap _inputs;
+    float currentTime;
 
     void Awake()
     {
@@ -25,8 +27,23 @@ public class DisplayTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*if (txt)
+            txt.text = System.TimeSpan.FromSeconds((int)TimerManager.Instance.time).ToString();*/
+
+        currentTime += Time.deltaTime;
         if (txt)
-            txt.text = System.TimeSpan.FromSeconds((int)TimerManager.Instance.time).ToString();
+            txt.text = FormatTime(currentTime);
+    }
+
+    string FormatTime(float time)
+    {
+        int intTime = (int)time;
+        int minutes = intTime / 60;
+        int seconds = intTime % 60;
+        float fraction = time * 1000;
+        fraction = (fraction % 1000);
+        string timeText = String.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
+        return timeText;
     }
 
     private void EnableScore()
--- Assets/Scripts/Timer/PrintTimerMenu.cs
+++ Assets/Scripts/Timer/PrintTimerMenu.cs
@@ -10,6 +10,17 @@ public class PrintTimerMenu : MonoBehaviour
     void Awake()
     {
         txt = GetComponent<TextMeshProUGUI>();
-        txt.text = "Best time:\n" + TimeSpan.FromSeconds((int)TimerManager.Instance.bestTime[index]).ToString();
+       // txt.text = "Best time:\n" + TimeSpan.FromSeconds((int)TimerManager.Instance.bestTime[index]).ToString();
+        txt.text = "Best time:\n" + FormatTime(TimerManager.Instance.bestTime[index]);
+    }
+    string FormatTime(float time)
+    {
+        int intTime = (int)time;
+        int minutes = intTime / 60;
+        int seconds = intTime % 60;
+        float fraction = time * 1000;
+        fraction = (fraction % 1000);
+        string timeText = String.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
+        return timeText;
     }
 }
